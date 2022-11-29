1057b9021bc9e3bfc03e960a2f49b2518fffe0b7
gabriel.strieder <gabriel.strieder@assurant.com>
2022-11-28 22:27:26 -0300

    layout last version

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 export function Post() {
@@ -5,8 +6,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
+          <Avatar
             src="https://scontent.fgel2-1.fna.fbcdn.net/v/t1.6435-9/33944141_1873258072704642_6310871631589801984_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFOW46fhQ_FmsmwMS8Gh_08Oapxe_rp-uk5qnF7-un66eptFLUQgAw-jdkPgE06BwU_iHin72eYQQ0F-KM2T9eQ&_nc_ohc=CetXC_kFaBUAX-k3bOq&_nc_ht=scontent.fgel2-1.fna&oh=00_AfCShH-RiovF6nOHRj10gxhJIBLaJn9qRGRvvJf61H3Q7w&oe=63A6F4E6"
             alt=""
           />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Sidebar.module.css";
 export function Siderbar() {
   return (
@@ -9,11 +10,7 @@ export function Siderbar() {
         alt=""
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://scontent.fgel2-1.fna.fbcdn.net/v/t1.6435-9/33944141_1873258072704642_6310871631589801984_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFOW46fhQ_FmsmwMS8Gh_08Oapxe_rp-uk5qnF7-un66eptFLUQgAw-jdkPgE06BwU_iHin72eYQQ0F-KM2T9eQ&_nc_ohc=CetXC_kFaBUAX-k3bOq&_nc_ht=scontent.fgel2-1.fna&oh=00_AfCShH-RiovF6nOHRj10gxhJIBLaJn9qRGRvvJf61H3Q7w&oe=63A6F4E6"
-          alt=""
-        />
+        <Avatar src="https://scontent.fgel2-1.fna.fbcdn.net/v/t1.6435-9/33944141_1873258072704642_6310871631589801984_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFOW46fhQ_FmsmwMS8Gh_08Oapxe_rp-uk5qnF7-un66eptFLUQgAw-jdkPgE06BwU_iHin72eYQQ0F-KM2T9eQ&_nc_ohc=CetXC_kFaBUAX-k3bOq&_nc_ht=scontent.fgel2-1.fna&oh=00_AfCShH-RiovF6nOHRj10gxhJIBLaJn9qRGRvvJf61H3Q7w&oe=63A6F4E6" />
         <strong>Gabriel Ten Caten</strong>
         <span>Full Stack Developer</span>
       </div>
